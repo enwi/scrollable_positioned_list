5454fd7dde016d757250af0ce47b6158230dee53
Moritz Wirger <lj.moritorii@web.de>
2025-12-17 08:09:53 +0100

    Fix some warnings

--- lib/src/scrollable_positioned_list.dart
+++ lib/src/scrollable_positioned_list.dart
@@ -144,6 +144,7 @@ class ScrollablePositionedList extends StatefulWidget {
   ///  Defaults to false.
   ///
   /// See [ScrollView.shrinkWrap].
+  /// {@endtemplate}
   final bool shrinkWrap;
 
   /// How the scroll view should respond to user input.
